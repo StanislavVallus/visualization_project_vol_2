36c13a5b4f87f498ffab7be0ba87db1cdb0ec2d0
StanislavVallus <stanko.vallus@gmail.com>
2025-12-23 10:42:12 +0100

    Update data path for energy data CSV

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ const tooltip = d3.select('#tooltip');
 
 // problem with paths - editing (stanislav)
 // const dataPath = '../data/owid-energy-data.csv';
-const dataPath = 'data/owid-energy-data.csv';
+const dataPath = 'visualization_project_vol_2/data/owid-energy-data.csv';
 let dataStore = null;
 let mapReady = null;
 let playTimer = null;
